4ca112d6cec31e1e4c5570fda49b29098adcd703
Tyler Church <tylertrain@yahoo.com>
2009-02-19 18:34:05 -0700

    added a bullet class

--- game.py
+++ game.py
@@ -16,13 +16,15 @@ class Player(box.Box):
         def __init__(self):
                 box.Box.__init__(self)
                 self.speed = 5
-                self.name = 'Player'
-         #def draw(self)
-        def update(self):
+                self.name = 'Player'	#def draw(self)
+	def update(self):
                 if pygame.key.get_pressed()[K_UP]: self.y -= self.speed
 		if pygame.key.get_pressed()[K_DOWN]: self.y += self.speed
 		if pygame.key.get_pressed()[K_LEFT]: self.x -= self.speed
 		if pygame.key.get_pressed()[K_RIGHT]: self.x += self.speed
+		if pygame.key.get_pressed()[K_SPACE]: 
+			if Bullet.numOf < 1:
+				Bullet()
 
 class Target(box.Box):
         def __init__(self):
@@ -30,6 +32,23 @@ class Target(box.Box):
         def collision(self,obj):
                 if obj.name == 'Player':
                         self.manager.destroy(self)
+
+class Bullet(box.Box):
+	numOf = 0
+	def __init__(self,pos=(50,50),velocity=(5,0),color=THECOLORS['blue']):
+		box.Box.__init__(self,pos=pos,size=(5,5),boxcolor=color)
+		self.speed = 10
+		self.name = 'Bullet'
+		self.velocity = velocity
+		Bullet.numOf += 1
+		
+	def update(self):
+		self.x += self.velocity[0]
+		self.y += self.velocity[1]
+		
+	def destroy(self):
+		Bullet.numOf -= 1
+
 def main():
         world = manager.World()
         Player()
